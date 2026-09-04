813142c8a0338f01b813d764235ea03f2bffca19
Kim <kimg.cose.swu@phinmaed.com>
2026-09-04 16:33:25 +0800

    added the welcome activity

--- app/src/main/java/com/example/biyahe/WelcomeActivity.kt
+++ app/src/main/java/com/example/biyahe/WelcomeActivity.kt
@@ -1,4 +1,21 @@
 package com.example.biyahe
 
-class WelcomeActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val btnGetStarted = findViewById<Button>(R.id.btnGetStarted)
+
+        btnGetStarted.setOnClickListener {
+            val intent = Intent(this, SignupActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
